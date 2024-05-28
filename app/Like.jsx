469cf1a0b225368ad500299f955aaa68a8887f4d
Benson Chen <tumlivein@gmail.com>
2024-05-29 01:39:42 +0800

    feat: able to play with client component with webpack hack

--- app/Like.jsx
+++ app/Like.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 export default function Like() {
     const [likes, setLikes] = useState(0);
--- app/_client.jsx
+++ app/_client.jsx
@@ -2,6 +2,15 @@ import { createRoot } from "react-dom/client";
 // fetch stream and convert it to html
 import { createFromFetch } from 'react-server-dom-webpack/client';
 
+// HACK: map webpack resolution to native ESM
+// @ts-expect-error Property '__webpack_require__' does not exist on type 'Window & typeof globalThis'.
+// if browser try to look at the window in order to import the module
+// don't do that, just import it straight from the server
+window.__webpack_require__ = async (id) => {
+    // we can use import in the browser 
+    return import(id);
+};
+
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
